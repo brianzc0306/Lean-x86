aba7c62c3c2e1c66d8a60226b897d00a1324f57d
brianzc0306 <48046657+brianzc0306@users.noreply.github.com>
2024-12-09 17:57:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,4 +22,4 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-git clone https://github.com/gdy666/luci-app-lucky.git package/lean/luci-app-lucky
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
